344ff18c7a4fbd531b8b3d4d7c56fb98e331e2b8
Leonardo <lrodriguesdasilva@gmail.com>
2020-05-17 23:04:59 -0300

    Limpeza da branch master

--- src/main/java/br/com/caixa/sirfg/repository/AmbienteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.caixa.sirfg.repository;
-
-import br.com.caixa.sirfg.model.Ambiente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AmbienteRepository extends JpaRepository<Ambiente, Long> {
-}
